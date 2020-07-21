251f5500f79e3e86f7b66e65ce4af532569e7372
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2020-07-22 01:13:34 +0900

    add demo API

---
+++ src/graphql/mutations.ts
@@ -0,0 +1,52 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createRoom = /* GraphQL */ `
+  mutation CreateRoom(
+    $input: CreateRoomInput!
+    $condition: ModelRoomConditionInput
+  ) {
+    createRoom(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateRoom = /* GraphQL */ `
+  mutation UpdateRoom(
+    $input: UpdateRoomInput!
+    $condition: ModelRoomConditionInput
+  ) {
+    updateRoom(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteRoom = /* GraphQL */ `
+  mutation DeleteRoom(
+    $input: DeleteRoomInput!
+    $condition: ModelRoomConditionInput
+  ) {
+    deleteRoom(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
