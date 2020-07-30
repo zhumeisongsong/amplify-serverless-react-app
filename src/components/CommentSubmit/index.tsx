2028cbef5218703226dd9e22a6f62a3c48fe15da
renyi.chen <823100926@qq.com>
2020-07-30 18:23:18 +0800

    fix: KGN-55

--- src/components/CommentSubmit/index.tsx
+++ src/components/CommentSubmit/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 import { CommentForm } from './style';
 import { TopProps } from '../../containers/Top';
 
 export default ({ createComment, toggleLoadNew }: TopProps) => {
   const [form] = Form.useForm();
+  const [isfocus, setIsFocus] = useState(false);
 
   const onFinish = (values: any) => {
     if (createComment) {
       createComment(values);
 
       form.resetFields();
+      setIsFocus(true);
 
       if (toggleLoadNew) {
         toggleLoadNew(true);
@@ -29,7 +31,7 @@ export default ({ createComment, toggleLoadNew }: TopProps) => {
         onFinish={onFinish}
       >
         <Form.Item name="content" normalize={(value) => value.trim()} rules={[{ required: true }, { max: 100 }]}>
-          <Input placeholder="コメント入力してください" />
+          <Input placeholder="コメント入力してください" autoFocus={isfocus} />
         </Form.Item>
 
         <Button htmlType="submit" style={{opacity: form.getFieldValue('content') ? 1 : 0.7}}>送る</Button>
